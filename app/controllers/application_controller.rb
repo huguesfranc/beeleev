c96150fe0addf55998fb26b8e8c98244a25d58ff
Julien Dargelos <julien.dargelos@me.com>
2018-04-13 14:09:49 +0200

    Confgurin staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  if Rails.env.production? && !ENV["LIVE"]
-    http_basic_authenticate_with :name => "beeleev", :password => "beeleev"
+  if Rails.env.staging?
+    http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'], password: ENV['HTTP_AUTH_PASSWORD']
   end
 
   @navbar_type = "white"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,10 +46,10 @@ Rails.application.configure do
     domain:               'heroku.com',
     enable_starttls_auto: true
   }
+end
 
-  Cloudinary.config do |config|
+Cloudinary.config do |config|
     config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
     config.api_key = ENV['CLOUDINARY_API_KEY']
     config.api_secret = ENV['CLOUDINARY_API_SECRET']
-  end
 end
