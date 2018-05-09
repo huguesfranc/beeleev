3693c5d71f64153044679d3f50933d3c7b165d35
Julien Dargelos <julien.dargelos@me.com>
2018-05-09 12:34:58 +0200

    #29

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -8,28 +8,28 @@ class UserDecorator < Draper::Decorator
   def network
   	connections_received + connections_sent
   end
-  
+
   def connections_received
-  	user2_connections.map(&:user1)
+  	user2_connections.map &:user1
   end
 
   def connections_sent
-  	user1_connections.map(&:user2)
+  	user1_connections.map &:user2
   end
 
   def full_capitalized_name
-  	first_name.upcase + " " + last_name.upcase
+  	first_name.to_s.upcase + " " + last_name.to_s.upcase
   end
 
   def full_titleized_name
-    first_name.titleize + " " + last_name.titleize
+    first_name.to_s.titleize + " " + last_name.to_s.titleize
   end
 
   def position_at_company
-    position + " @" + company
+    position.to_s + " @" + company.to_s
   end
 
   def all_spoken_languages
-    spoken_languages.map(&:capitalize).sort.join(", ")
+    spoken_languages.map{ |spoken_language| spoken_language.to_s.capitalize }.sort.join ', '
   end
 end
