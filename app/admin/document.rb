ff21dcab2d11489cf29abd96054f338ea385343d
Julien Dargelos <julien.dargelos@me.com>
2018-05-14 20:14:48 +0200

    Fixing admin document

--- app/admin/document.rb
+++ app/admin/document.rb
@@ -1,13 +1,31 @@
 ActiveAdmin.register Document do
   permit_params :name, :description, :file
 
-  filter :email
-
   index do
     selectable_column
     id_column
     column :name
     actions
+  end
+
+  controller do
+    def create
+      super do |format|
+        redirect_to admin_document_path(resource), notice: resource.valid? ? "Document created" : "Could not create document"
+      end
+    end
+
+    def update
+      super do |format|
+        redirect_to admin_document_path(resource), notice: resource.valid? ? "Document updated" : "Could not update document"
+      end
+    end
+
+    def destroy
+      super do |format|
+        redirect_to admin_documents_path, notice: "Document destroyed"
+      end
+    end
   end
 
   show do
