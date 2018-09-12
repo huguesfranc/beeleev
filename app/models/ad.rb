3ff20a8785d83b8d38e51b4a7bbcfb804859fa87
Olivier Crété <ocrete@sfrpims.fr>
2018-09-12 13:59:20 +0200

    Add fields description to ad model

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,3 +1,15 @@
 class Ad < ActiveRecord::Base
+=begin
+  id: integer,
+  ad_type: string,
+  user_id: integer,
+  ad_content: text,
+  illustration: string,
+  ad_link: string,
+  click: integer,
+  title: string,
+  created_at: datetime,
+  updated_at: datetime,
+=end
   belongs_to :user
 end
