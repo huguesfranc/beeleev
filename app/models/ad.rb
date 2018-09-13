0cb71dde8e717d01f8e1b016d66f40ce2bfab66d
Olivier Crété <ocrete@sfrpims.fr>
2018-09-13 11:19:28 +0200

    Ad model information

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,16 +1,16 @@
+# == Schema Information
+# id: integer,
+# ad_type: string,
+# user_id: integer,
+# ad_content: text,
+# illustration: string,
+# ad_link: string,
+# click: integer,
+# title: string,
+# created_at: datetime,
+# updated_at: datetime,
+#
 class Ad < ActiveRecord::Base
-=begin
-  id: integer,
-  ad_type: string,
-  user_id: integer,
-  ad_content: text,
-  illustration: string,
-  ad_link: string,
-  click: integer,
-  title: string,
-  created_at: datetime,
-  updated_at: datetime,
-=end
   belongs_to :user
 
   validates :title, presence: true
